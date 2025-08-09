869d5080afe9c42b3f4f1fa9c53a83b0b5f8beb2
Juan Camilo Narváez <ulvenforst@gmail.com>
2025-08-09 16:21:02 -0500

    Updates

--- assets/input-Zrtw-VZs.js
+++ assets/input-Zrtw-VZs.js
@@ -1,4 +1,4 @@
-import{_ as t,j as n,$ as o}from"./index-C3vuwuWh.js";/**
+import{_ as t,j as n,$ as o}from"./index-pEJXSL1S.js";/**
  * @license lucide-react v0.532.0 - ISC
  *
  * This source code is licensed under the ISC license.
